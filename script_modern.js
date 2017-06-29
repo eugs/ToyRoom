02185880807c56309d758e9746733e53f90952b2
Eugen Stotik <eugen_stotik@epam.com>
2017-06-29 13:42:02 +0300

    Create script for modern JS inheritance (mixing creatures to chimera)
    q
    :q

---
+++ script_modern.js
@@ -0,0 +1,85 @@
+function mix() {
+  var arg, prop, child = {};
+
+  for(arg = 0; arg<arguments.length; arg+=1) {
+    for(prop in arguments[arg]) {
+      if(arguments[arg].hasOwnProperty(prop)) {
+
+        //cloning property
+        addProperty(arguments[arg], prop, child, "");
+
+      }
+    }
+  }
+  return child;
+}
+
+function addProperty(parent, prop, child, index) {
+  if(child[prop+index]) {
+    index = (index) ? index : "0";
+    addProperty(parent, prop, child, parseInt(index)+1);
+  } else {
+    child[prop+index] = parent[prop];
+  }
+}
+
+
+//CREATURES
+
+//FROG
+var frog = {
+  legs: 4,
+  quack: true,
+  name: "FROG"
+};
+frog.say = function () {
+  console.log("QUACK");
+};
+frog.swim = function() {
+  console.log("I'm swimming!");
+}
+
+//SNAKE
+var snake = {
+  name: "SNAKE",
+  legs: false,
+  tongue: "long",
+  poison: true
+};
+snake.say = function () {
+  console.log("SHHHHH");
+}
+snake.crawl = function() {
+  console.log("crawling");
+}
+
+//LIZARD
+var lizard = {
+  name: "LIZARD",
+  legs: 4,
+  skin: "green",
+
+  regenerate: function() {
+    console.log("I have tail again!");
+  },
+  say: function () {
+    console.log("silence...");
+  },
+  sneak: function() {
+    console.log("sneaking...")
+  }
+};
+
+var chimera = mix(frog, snake, lizard);
+console.log(chimera);
+
+console.log("\n------\nCHIMERA CAN:");
+chimera.regenerate();
+chimera.sneak();
+chimera.crawl();
+chimera.swim();
+
+console.log("\n-------\nchimera says:");
+chimera.say();
+chimera.say1();
+chimera.say2();
